06709930728f0520ab178b7024fa228a24d806f6
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2024-10-06 17:30:17 -0300

    fazendo as funcionalidades das setas

--- scripts/index.js
+++ scripts/index.js
@@ -24,36 +24,49 @@ var dataJson = [
         "descricao": "O sorvete em versão de café pode ser aproveitado sozinho ou com um pedacinho de bolo no fim..."
     }
 ]
-
+var minId = 0
+var maxId = 2
+var carrossel = []
+// window.addEventListener("resize", function(){
+//     
+// })
 
 function clickEsquerdo() {
-    console.log("esquerda")
+    deleteCards()
+    carrossel = carrossel.map(e => +e-1 <= -1 ? 3 : +e-1)
+    carrossel.map(i => {dataJson.forEach(e => {e.id == i ? criarCard(i) : undefined})}) 
 }
 
 function clickDireito(){
-    console.log("direita")
+    deleteCards()
+    carrossel = carrossel.map(e => +e+1 >= 4 ? 0 : +e+1)
+    carrossel.map(i => {dataJson.forEach(e => {e.id == i ? criarCard(i) : undefined})})  
 }
 
-function criarCard(id, contador){
+function deleteCards(){
+    const cardContainer = document.getElementById("cards-id")
+    if (carrossel.length > 0) carrossel = []
+    for(child of cardContainer.children){ carrossel.push((child.id).match(/\d/g).join(""))}
+    cardContainer.replaceChildren("")
+}
+
+function criarCard(id){
     const cardContainer = document.getElementById('cards-id')
     const cardTemplate = document.createElement('div')
     cardTemplate.classList.add('card')
-
+    cardTemplate.id = `card-${id}`
     cardTemplate.innerHTML = `
-    <div id="card-${contador}">
-        <img id="img-${contador}" class="imagemCard" src="${dataJson[id].foto}" alt="${dataJson[id].titulo}">
+        <img id="img-${id}" class="imagemCard" src="${dataJson[id].foto}" alt="${dataJson[id].titulo}">
         <div>
-            <div id="titulo-${contador}" class="tituloCard"><h3>${dataJson[id].titulo}</h3></div>
-            <div id="descricao-${contador}" class="textoCard"><p>${dataJson[id].descricao}</p></div>
+            <div id="titulo-${id}" class="tituloCard"><h3>${dataJson[id].titulo}</h3></div>
+            <div id="descricao-${id}" class="textoCard"><p>${dataJson[id].descricao}</p></div>
         </div>
-    </div>`;
+    `;
     cardContainer.appendChild(cardTemplate);
 }
 
 window.onload = function() {
-    contador = 0;
     dataJson.forEach(e => {
-        contador++;
-        if(e.id < 3) criarCard(e.id, contador);
+        if(e.id >= minId && e.id <= maxId) criarCard(e.id);
     })
 }
